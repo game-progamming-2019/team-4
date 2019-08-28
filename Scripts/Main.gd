30e4c9c0924b41f4cfb837151f83ebacd8e93842
LukiwiCoding <SchwartzLukas@t-online.de>
2019-08-28 17:27:28 +0200

    removed array of outlines from detected players

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -8,10 +8,6 @@ var levelsArr = [
 	"res://Scenes/Levels/Level_2.tscn",
 	"res://Scenes/Levels/Level_3.tscn"
 	]
-var gameOvers = [
-
-]
-
 var outline = "res://Assets/Sprites/Outline.png"
 
 func _ready():
@@ -46,7 +42,6 @@ func _on_player_detected():
 		detected.texture = load(outline)
 		detected.position = player.position
 		get_node("Levels/Level_" + str(currentLevel) + "/PlayerResets").add_child(detected)
-		gameOvers.append(detected)
 		player.position = $SpawnPoint.position 
 		print("detected, game over")
 	pass
